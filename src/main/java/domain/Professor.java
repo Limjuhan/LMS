d1f64f4aee21e0d5c3467126bf7b1ca6ee984691
Limjuhan <starlexy@naver.com>
2025-05-11 02:03:36 +0900

    -. 대학구분을 위한 dept테이블 컬럼추가 -. 대학구분 select박스 구현 완료

---
+++ src/main/java/domain/Professor.java
@@ -0,0 +1,32 @@
+package domain;
+
+import java.util.Date;
+
+public class Professor {
+    private String professorId;
+    private String professorName;
+    private String professorEmail;
+    private Date professorBirthday;
+    private String professorPhone;
+    private String professorMajor;
+    private String professorImg;
+    private String professorPassword;
+
+    // Getters and Setters
+    public String getProfessorId() { return professorId; }
+    public void setProfessorId(String professorId) { this.professorId = professorId; }
+    public String getProfessorName() { return professorName; }
+    public void setProfessorName(String professorName) { this.professorName = professorName; }
+    public String getProfessorEmail() { return professorEmail; }
+    public void setProfessorEmail(String professorEmail) { this.professorEmail = professorEmail; }
+    public Date getProfessorBirthday() { return professorBirthday; }
+    public void setProfessorBirthday(Date professorBirthday) { this.professorBirthday = professorBirthday; }
+    public String getProfessorPhone() { return professorPhone; }
+    public void setProfessorPhone(String professorPhone) { this.professorPhone = professorPhone; }
+    public String getProfessorMajor() { return professorMajor; }
+    public void setProfessorMajor(String professorMajor) { this.professorMajor = professorMajor; }
+    public String getProfessorImg() { return professorImg; }
+    public void setProfessorImg(String professorImg) { this.professorImg = professorImg; }
+    public String getProfessorPassword() { return professorPassword; }
+    public void setProfessorPassword(String professorPassword) { this.professorPassword = professorPassword; }
+}
